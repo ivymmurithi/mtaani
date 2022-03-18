1713e6f72ec96515a5201dd920b2637f7468a790
ivymmurithi <ivymurithi@gmail.com>
2022-03-18 22:35:06 +0300

    add models to the administration page

--- mtaaniapp/admin.py
+++ mtaaniapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Neighbourhood,Post,Business,Profile,Contact
 
 # Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Business)
+admin.site.register(Contact)
