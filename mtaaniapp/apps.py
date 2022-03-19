debf7eb358e17e41e6cb8e67b1d2ccb49f19857b
ivymmurithi <ivymurithi@gmail.com>
2022-03-19 12:23:08 +0300

    create profile after a user has been saved

--- mtaaniapp/apps.py
+++ mtaaniapp/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MtaaniappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mtaaniapp'
+
+    def ready(self):
+        import mtaaniapp.signals
