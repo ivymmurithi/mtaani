abff5b9f0dc2453c64d2b6aeef3db99d4326fd16
ivymmurithi <ivymurithi@gmail.com>
2025-08-11 21:59:29 +0300

    fix static url

--- mtaani/settings.py
+++ mtaani/settings.py
@@ -120,11 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 if not DEBUG:
     ALLOWED_HOSTS = ["mtaani.onrender.com"]
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 else:
