26d3dceba99cc13ef84948a5d5b962fb0adf0795
ivymmurithi <ivymurithi@gmail.com>
2025-08-12 11:48:16 +0300

    add localhost to allowed hosts

--- mtaani/settings.py
+++ mtaani/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ["mtaani.onrender.com"]
+ALLOWED_HOSTS = ["mtaani.onrender.com", "127.0.0.1"]
 
 # Application definition
 
